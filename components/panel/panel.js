d565d6ec2410b168c03b6dd37fba257d54359c7f
Dan Shearmur <dan.shearmur@gmail.com>
2015-10-27 21:12:52 +0000

    kind of working tabs

--- components/panel/panel.js
+++ components/panel/panel.js
@@ -1,16 +1,14 @@
 import template from 'lodash.template';
 
 export default template(`
-  <div class="Panel<% if (selected) { %> Panel--selected<% } %>" role="tabpanel" aria-labelledby="<%- panelId %>">
-    <ol class="Panel-list Panel-list--<%- id %>">
-      <% items.forEach(function (item) { %>
-        <li class="Panel-item">
-          <a class="Panel-itemLink" href="<%= item.webUrl %>">
-            <h3 class="Panel-webTitle"><%- item.webTitle %></h3>
-            <% if (item.fields) { %><div class="Panel-trailText"><%= item.fields.trailText %></div><% } %>
-          </a>
-        </li>
-      <% }); %>
-    </ol>
-  </div>
+  <ol class="Panel-list Panel-list--<%- id %>">
+    <% items.forEach(function (item) { %>
+      <li class="Panel-item">
+        <a class="Panel-itemLink" href="<%= item.webUrl %>">
+          <h3 class="Panel-webTitle"><%- item.webTitle %></h3>
+          <% if (item.fields) { %><div class="Panel-trailText"><%= item.fields.trailText %></div><% } %>
+        </a>
+      </li>
+    <% }); %>
+  </ol>
 `);
