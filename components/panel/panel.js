19a1a0343e5ba02746deaa3e88927904ce072933
Dan Shearmur <dan.shearmur@gmail.com>
2015-10-27 21:03:31 +0000

    Some content

--- components/panel/panel.js
+++ components/panel/panel.js
@@ -1,14 +1,16 @@
 import template from 'lodash.template';
 
 export default template(`
-  <div role="tabpanel" aria-labelledby="<%= panelId %>">
-    <ul>
+  <div class="Panel<% if (selected) { %> Panel--selected<% } %>" role="tabpanel" aria-labelledby="<%- panelId %>">
+    <ol class="Panel-list Panel-list--<%- id %>">
       <% items.forEach(function (item) { %>
-        <li>
-          <a href="<%= item.webUrl %>"><%= item.webTitle %><% if (item.fields) { %>, <%- item.fields.trailText %><% } %></a>
+        <li class="Panel-item">
+          <a class="Panel-itemLink" href="<%= item.webUrl %>">
+            <h3 class="Panel-webTitle"><%- item.webTitle %></h3>
+            <% if (item.fields) { %><div class="Panel-trailText"><%= item.fields.trailText %></div><% } %>
+          </a>
         </li>
       <% }); %>
-    </ul>
+    </ol>
   </div>
 `);
-
--- components/tabs/tabs.js
+++ components/tabs/tabs.js
@@ -4,7 +4,7 @@ export default template(`
   <ul class="Tabs">
     <% tabs.forEach(function (tab) { %>
       <li class="Tabs-item<% if (tab.selected) { %> Tabs-item--selected<% } %>" role="presentation">
-        <a class="Tabs-itemLink" href="" role="tab" aria-controls="<%- tab.panelId %>" aria-selected="<%= tab.selected %>"><%= tab.section %></a>
+        <a class="Tabs-itemLink" href="" role="tab" aria-controls="<%- tab.panelId %>" aria-selected="<%- tab.selected %>"><%- tab.section %></a>
       </li>
     <% }); %>
   </ul>
